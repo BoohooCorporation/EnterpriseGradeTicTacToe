7746f13a39edff9aacaf465cf7d691a39f94b0b3
Neeron Bhatta <33238007+NeuronButter@users.noreply.github.com>
2022-11-10 12:09:46 +1100

    Initial Commit 2

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  // import svelteLogo from './assets/svelte.svg'
+  import Grid from './lib/Grid.svelte'
+</script>
+
+<main>
+  <h1>Tic Tac Toe</h1>
+
+  <div class="card">
+    <Grid />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="muted">
+    Made by Andrew and Neeron
+  </p>
+</main>
+
+<style>
+  .muted {
+    color: #888;
+  }
+</style>
