01e10fe8227404ac44ff36a15ff90f1eca0633c0
Neeron Bhatta <33238007+NeuronButter@users.noreply.github.com>
2022-11-10 12:09:37 +1100

    Initial Commit

---
+++ src/lib/Grid.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  // let count: number = 0
+  // const increment = () => {
+  //   count += 1
+  // }
+</script>
+
+<!-- <button on:click={increment}>
+  count is {count}
+</button> -->
+
+<!-- tic tac toe grid using tailwind -->
+<div class="grid grid-cols-3 grid-rows-3 gap-4">
+  <div class="border-2 border-black bg-white">1</div>
+  <div class="border-2 border-black bg-white">2</div>
+  <div class="border-2 border-black bg-white">3</div>
+  <div class="border-2 border-black bg-white">4</div>
+  <div class="border-2 border-black bg-white">5</div>
+  <div class="border-2 border-black bg-white">6</div>
+  <div class="border-2 border-black bg-white">7</div>
+  <div class="border-2 border-black bg-white">8</div>
+  <div class="border-2 border-black bg-white">9</div>
+</div>
