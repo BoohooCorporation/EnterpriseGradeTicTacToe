14ba09169f0767da82c7f40bd1d075fef644f643
Neeron Bhatta <33238007+NeuronButter@users.noreply.github.com>
2022-11-22 09:26:23 +1100

    10% chance of going to Elon Musk's Twitter

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,9 @@ const app = new App({
   target: document.getElementById('app')
 })
 
+//random chance 20%
+if (Math.random() > 0.9) {
+  window.location.href = 'https://twitter.com/elonmusk'
+}
+
 export default app
